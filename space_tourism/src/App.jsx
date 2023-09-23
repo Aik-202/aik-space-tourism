375fe1dfea626a4f7421441f3cca01a634626ed2
Aik-202 <ikogweassurance85@gmail.com>
2023-09-23 11:13:51 +0100

    Added routes, now working on the home page

--- space_tourism/src/App.jsx
+++ space_tourism/src/App.jsx
@@ -1,4 +1,29 @@
-// import { useState } from 'react'
+// import {
+//     BrowserRouter as Router,
+//     Routes,
+//     Route
+// } from "react-router-dom";
+import {
+    Home,
+    Crew,
+    Destination,
+    Technology
+
+} from './Pages'
 import './App.css'
 
-export default function App() {}
+export default function App() {
+    return (
+        <Home />
+        // <>
+        //     <Router>
+        //         <Routes>
+        //             <Route exact path="/" element={<Home />} ></Route>
+        //             <Route exact path="/destination" element={<Destination />} ></Route>
+        //             <Route exact path="/crew" element={<Crew />} ></Route>
+        //             <Route exact path="/technology" element={<Technology />} ></Route>
+        //         </Routes>
+        //     </Router>
+        // </>
+    )
+}
--- space_tourism/src/Pages/Home.jsx
+++ space_tourism/src/Pages/Home.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
+import {HomeButton, HomeText} from '../Components'
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <section>
+      <HomeText />
+      <HomeButton />
+    </section>
   )
 }
