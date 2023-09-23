fef7ca1d25dce2058f25de2a7b9f7b7268f16024
Aik-202 <ikogweassurance85@gmail.com>
2023-09-23 10:32:00 +0100

    Finished exporting all images

---
+++ space_tourism/src/Data/index.jsx
@@ -0,0 +1,41 @@
+export{default as CrewDesktop} from './images/crew/background-crew-desktop.jpg';
+export{default as CrewMobile} from './images/crew/background-crew-mobile.jpg';
+export{default as CrewTablet} from './images/crew/background-crew-tablet.jpg';
+export{default as AnoushehPng} from './images/crew/image-anousheh-ansari.png';
+export{default as AnoushehWeb} from './images/crew/image-anousheh-ansari.webp';
+export{default as DouglasPng} from './images/crew/image-douglas-hurley.png';
+export{default as DouglasWeb} from './images/crew/image-douglas-hurley.webp';
+export{default as MarkPng} from './images/crew/image-mark-shuttleworth.png';
+export{default as MarkWeb} from './images/crew/image-mark-shuttleworth.webp';
+export{default as VictorPng} from './images/crew/image-victor-glover.png';
+export{default as VictorWeb} from './images/crew/image-victor-glover.webp';
+
+export{default as DestinationDesktop} from './images/destination/background-destination-desktop.jpg';
+export{default as DestinationMobile} from './images/destination/background-destination-mobile.jpg';
+export{default as DestinationTablet} from './images/destination/background-destination-tablet.jpg';
+export{default as EuropaPng} from './images/destination/image-europa.png';
+export{default as EuropaWeb} from './images/destination/image-europa.webp';
+export{default as MarsPng} from './images/destination/image-mars.png';
+export{default as MarsWeb} from './images/destination/image-mars.webp';
+export{default as MoonPng} from './images/destination/image-moon.png';
+export{default as MoonWeb} from './images/destination/image-moon.webp';
+export{default as TitianPng} from './images/destination/image-titan.png';
+export{default as TitianWeb} from './images/destination/image-titan.webp';
+
+export{default as HomeDesktop} from './images/home/background-home-desktop.jpg';
+export{default as HomeMobile} from './images/home/background-home-mobile.jpg';
+export{default as HomeTablet} from './images/home/background-home-tablet.jpg';
+
+export{default as Close} from './images/shared/icon-close.svg';
+export{default as Hamburger} from './images/shared/icon-hamburger.svg';
+export{default as Logo} from './images/shared/logo.svg';
+
+export{default as TechnologyDesktop} from './images/technology/background-technology-desktop.jpg';
+export{default as TechnologyMobile} from './images/technology/background-technology-mobile.jpg';
+export{default as TechnologyTablet} from './images/technology/background-technology-tablet.jpg';
+export{default as LaunchLandScape} from './images/technology/image-launch-vehicle-landscape.jpg';
+export{default as LaunchPortrait} from './images/technology/image-launch-vehicle-portrait.jpg';
+export{default as CapsuleLandScape} from './images/technology/image-space-capsule-landscape.jpg';
+export{default as CapsulePortrait} from './images/technology/image-space-capsule-portrait.jpg';
+export{default as PortLandScape} from './images/technology/image-spaceport-landscape.jpg';
+export{default as PortPortrait} from './images/technology/image-spaceport-portrait.jpg';
