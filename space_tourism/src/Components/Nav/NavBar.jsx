ae754921158222435e3aef658ea7989e4f4bc7c7
Aik-202 <ikogweassurance85@gmail.com>
2023-09-23 12:45:04 +0100

    Menu bar for mobile now working

--- space_tourism/src/Components/Nav/NavBar.jsx
+++ space_tourism/src/Components/Nav/NavBar.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 import NavItems from './NavItems'
-import { Logo } from '../../Data'
+import { Logo, Hamburger } from '../../Data'
 
 export default function NavBar() {
+  const [menuActive, setMenuActive] = React.useState(false);
+
+  const menuOpen = () => {
+    setMenuActive(true)
+  }
+
+  const menuClose = () => {
+    setMenuActive(false)
+  }
+
   return (
     <nav>
-      <figure>
-        <img src={Logo} alt="logo" />
+      <figure className="flex flex-row justify-between p-5 cursor-pointer">
+        <img src={Logo} alt="logo" className='self-center'/>
+        {!menuActive && <img src={Hamburger} alt="menu-bar" className="self-center" onClick={menuOpen}/>}
       </figure>
-      <NavItems />
+      <NavItems menuStatus={menuActive} close={menuClose}/>
     </nav>
   )
 }
--- space_tourism/src/Components/Nav/NavItems.jsx
+++ space_tourism/src/Components/Nav/NavItems.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
+import { Close } from '../../Data'
 
-export default function NavItems() {
+export default function NavItems(props) {
   return (
-    <ul>
+    <div className={`${props.menuStatus ? 'flex' : 'hidden'} flex-col absolute top-0 bottom-0 right-0 left-32 bg-[#fff] bg-opacity-5`}>
+      {props.menuStatus &&<figure className='self-end mt-8 mr-8'>
+        <img src={Close} alt="menu-bar" className="self-center" onClick={props.close}/>
+      </figure>}
+      <ul className='flex flex-col content-start space-y-10 flex-wrap text-[#fff] pl-10 pt-32'>
+      
       <li>
-        <span>00</span>
+        <span className='mr-4 font-extrabold'>00</span>
         Home
       </li>
       <li>
-        <span>01</span>
+        <span className='mr-4 font-extrabold'>01</span>
         Destination
       </li>
       <li>
-        <span>02</span>
+        <span className='mr-4 font-extrabold'>02</span>
         Crew
       </li>
       <li>
-        <span>03</span>
+        <span className='mr-4 font-extrabold'>03</span>
         Technology
       </li>
     </ul>
+    </div>
   )
 }
