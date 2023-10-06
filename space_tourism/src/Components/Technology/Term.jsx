aa64ae7b4e9626409e03d97ed09aa29b9f78e0f6
Aik-202 <ikogweassurance85@gmail.com>
2023-10-06 12:46:23 +0100

    Attended html and styles for technology

--- space_tourism/src/Components/Technology/Term.jsx
+++ space_tourism/src/Components/Technology/Term.jsx
@@ -1,7 +1,32 @@
 import React from 'react'
+import { technology } from '../../Data/demo';
+import {About} from '../'
 
-export default function Term() {
+export default function Term(props) {
+  window.onresize = () => {location.reload()}
   return (
-    <div>Term</div>
+    <section className='flex flex-col flex-wrap content-center items-center justify-center space-y-5 sm:space-y-10 xl:items-start xl:content-start xl:justify-start'>
+      <h1 className='text-white font-font-two uppercase tracking-widest mb-5 self-start ml-10 sm:text-[20px] lg:text-[23px] xl:ml-40 xl:mt-5'>
+        <span className='tracking-widest mr-4 font-extrabold opacity-[15%]'>03</span>
+        Space Launch 101 
+      </h1>
+      <div className='flex flex-col-reverse sm:flex-col lg:flex-row lg:space-x-32 lg:ml-10 xl:ml-40 xl:-space-x-10'>
+      <About techActive={props.techActive} change={props.change}/>
+        <div className='flex flex-col space-y-5 sm:space-y-7 lg:space-y-0 lg:flex-row lg:space-x-24 xl:space-x-40 xl:mx-18'>
+          {technology.map(item => 
+            <figure key={item.images.portrait} className=
+            {`
+              ${item.name =="Launch vehicle" && props.techActive == "Launch" ? 'block':
+              item.name =="Spaceport" && props.techActive == "Port" ? 'block':
+              item.name =="Space capsule" && props.techActive == "Capsule" ? 'block':
+              'hidden'
+              } flex flex-col`}
+            >
+              <img src={window.screen.availWidth <= 912 ? item.images.landscape : item.images.portrait } alt={item.name} className='self-center w-full lg:w-[20rem]'/>
+            </figure>
+          )}
+        </div>
+      </div>
+    </section>
   )
 }
--- space_tourism/src/Pages/Technology.jsx
+++ space_tourism/src/Pages/Technology.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import {NavBar} from '../Components'
+import {NavBar, Term} from '../Components'
 
 export default function Technology() {
+  const [techActive, settechActive] = React.useState("Launch");
+
+  const techChange = (e) => {
+    if (e.target.id == "Launch vehicle") {
+      settechActive("Launch")
+    } 
+    if (e.target.id == "Spaceport") {
+      settechActive("Port")
+    } 
+    if (e.target.id == "Space capsule") {
+      settechActive("Capsule")
+    }  
+  }
   return (
     <section className='home w-full h-full bg-technology-mobile sm:bg-technology-tablet xl:bg-technology-desktop bg-no-repeat 
     bg-cover'>
       <NavBar homeActive={false} destActive = {false} crewActive= {false} techActive={true} for={"main"}/>
-      <div>Technology</div>
+      <Term techActive={techActive} change={techChange}/>
     </section>
   )
 }
