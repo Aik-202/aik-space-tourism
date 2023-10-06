de959d90a9e80a48d703c31556c9b432273796c0
Aik-202 <ikogweassurance85@gmail.com>
2023-10-06 09:04:34 +0100

    Trying to fix image problem

--- space_tourism/tailwind.config.js
+++ space_tourism/tailwind.config.js
@@ -7,21 +7,21 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'home-mobile': "url(./src/Data/images/home/background-home-mobile.jpg)",
-        'home-tablet': "url(./src/Data/images/home/background-home-tablet.jpg)",
-        'home-desktop': "url(./src/Data/images/home/background-home-desktop.jpg)",
+        'home-mobile': "url(/src/Data/images/home/background-home-mobile.jpg)",
+        'home-tablet': "url(/src/Data/images/home/background-home-tablet.jpg)",
+        'home-desktop': "url(/src/Data/images/home/background-home-desktop.jpg)",
 
-        'destination-mobile': "url(./src/Data/images/destination/background-destination-mobile.jpg)",
-        'destination-tablet': "url(./src/Data/images/destination/background-destination-tablet.jpg)",
-        'destination-desktop': "url(./src/Data/images/destination/background-destination-desktop.jpg)",
+        'destination-mobile': "url(/src/Data/images/destination/background-destination-mobile.jpg)",
+        'destination-tablet': "url(/src/Data/images/destination/background-destination-tablet.jpg)",
+        'destination-desktop': "url(/src/Data/images/destination/background-destination-desktop.jpg)",
 
-        'crew-mobile': "url(./src/Data/images/crew/background-crew-mobile.jpg)",
-        'crew-tablet': "url(./src/Data/images/crew/background-crew-tablet.jpg)",
-        'crew-desktop': "url(./src/Data/images/crew/background-crew-desktop.jpg)",
+        'crew-mobile': "url(/src/Data/images/crew/background-crew-mobile.jpg)",
+        'crew-tablet': "url(/src/Data/images/crew/background-crew-tablet.jpg)",
+        'crew-desktop': "url(/src/Data/images/crew/background-crew-desktop.jpg)",
 
-        'technology-mobile': "url(./src/Data/images/technology/background-technology-mobile.jpg)",
-        'technology-tablet': "url(./src/Data/images/technology/background-technology-tablet.jpg)",
-        'technology-desktop': "url(./src/Data/images/technology/background-technology-desktop.jpg)",
+        'technology-mobile': "url(/src/Data/images/technology/background-technology-mobile.jpg)",
+        'technology-tablet': "url(/src/Data/images/technology/background-technology-tablet.jpg)",
+        'technology-desktop': "url(/src/Data/images/technology/background-technology-desktop.jpg)",
 
       },
       colors: {
