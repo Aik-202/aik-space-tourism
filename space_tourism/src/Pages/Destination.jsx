536992e32076d70d2fcead9fbe1d00110d1e89fe
Aik-202 <ikogweassurance85@gmail.com>
2023-10-11 03:56:38 +0100

    Added some finishing touch

--- space_tourism/src/Pages/Destination.jsx
+++ space_tourism/src/Pages/Destination.jsx
@@ -3,7 +3,7 @@ import {NavBar, Planet, Music} from '../Components'
 
 export default function Destination() {
   return (
-    <section className='w-full h-full bg-destination-mobile sm:bg-destination-tablet xl:bg-destination-desktop bg-no-repeat 
+    <section className='h-max w-full pb-16 sm:pb-0 sm:h-full bg-destination-mobile sm:bg-destination-tablet xl:bg-destination-desktop bg-no-repeat 
     bg-cover flex flex-col sm:space-y-10'>
       <NavBar homeActive={false} destActive = {true} crewActive= {false} techActive={false} for={"main"}/>
       <Planet />
